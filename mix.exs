b15ecf819ab16b9e048226ecde82b59b1653341c
Franz Bettag <franz@bett.ag>
2020-11-15 22:22:08 +0100

    fixes some more error handling and does retrying for v0.0.8.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule RRPproxy.Mixfile do
   def project do
     [
       app: :rrpproxy,
-      version: "0.0.7",
+      version: "0.0.8",
       elixir: "~> 1.4",
       source_url: @project_url,
       homepage_url: @project_url,
